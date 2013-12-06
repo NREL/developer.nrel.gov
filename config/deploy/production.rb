a90b49e666ac5bacc4f6abfcf38ba314003d272d
Nick Muerdter <nick.muerdter@nrel.gov>
2013-12-06 09:23:30 -0700

    Setup deployment to secondary servers.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
 # Set the servers for this stage.
-role :app, "devprod-int.nrel.gov"
-role :web, "devprod-int.nrel.gov"
+role :app, "devprod-int.nrel.gov", "devprod-int2.nrel.gov"
+role :web, "devprod-int.nrel.gov", "devprod-int2.nrel.gov"
 
 # Set the base path for deployment.
 set :deploy_to_base, "/srv/data"
 
 # Set the accessible web domain for this site.
 set :base_domain, "developer.nrel.gov"
-set :base_domain_aliases, ["devprod-int.nrel.gov"]
+set :base_domain_aliases, ["devprod-int.nrel.gov", "devprod-int2.nrel.gov"]
 
 # Production-ready deployments should exclude git data.
 set :copy_exclude, [".git"]
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,15 +1,15 @@
 require "capistrano_nrel_ext/recipes/branches"
 
 # Set the servers for this stage.
-role :app, "devstage-int.nrel.gov"
-role :web, "devstage-int.nrel.gov"
+role :app, "devstage-int.nrel.gov", "devstage-int2.nrel.gov"
+role :web, "devstage-int.nrel.gov", "devstage-int2.nrel.gov"
 
 # Set the base path for deployment.
 set :deploy_to_base, "/srv/developer"
 
 # Set the accessible web domain for this site.
 set :base_domain, "devstage.nrel.gov"
-set :base_domain_aliases, ["devstage-int.nrel.gov"]
+set :base_domain_aliases, ["devstage-int.nrel.gov", "devstage-int2.nrel.gov"]
 
 # Production-ready deployments should exclude git data.
 set :copy_exclude, [".git"]
