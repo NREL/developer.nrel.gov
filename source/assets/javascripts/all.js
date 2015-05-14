5504aed46fdaee4e3b6e799423b221c7e5e0b133
Nick Muerdter <nick.muerdter@nrel.gov>
2015-05-14 16:50:42 -0600

    Fix broken table of contents on doc pages.

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,1 +1,9 @@
 //= require jquery/jquery.js
+//= require _vendor/jquery.tableofcontents.js
+
+$(document).ready(function() {
+  $('#toc').tableOfContents(document.body, {
+    startLevel: 2,
+    depth: 2
+  });
+});
