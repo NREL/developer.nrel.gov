edac95d092f02d4dfb2e763662ca64b4cfa34b22
Nick Muerdter <12112+GUI@users.noreply.github.com>
2025-04-28 20:09:13 -0600

    Various fixes and tweaks from feedback.
    - Various layout tweaks to better align with the latest NREL template on
      the live site (which differs from the published template this was
      based on). This includes header sizing tweaks, mobile placement of
      logo and search buttons, mobile nav menu sizing and alignment,
      breadcrumb separators, "Skip to main content" accessibility
      improvements, and other small tweaks.
    - Remove stylesheets that were unused and not referenced.
    - Adjust the styles for the "cards" to match the background color and
      non-rounded styles of the NREL site.
    - Better align the "APIs and Documentation" headers, breadcrumb name,
      and intro text.
    - Add intro text to signup form.
    - Remove duplicative signup link from home page.
    - Adjust footer links to match current production site (the old links
      to `.html` pages redirected to `www2.` domain).
    - Change "Contact Us" link in footer to go to our own site's contact
      page, rather than the NREL-wide one.
    - Inject some custom styles into the signup form's shadow DOM in order
      to have the links and buttons match our site-wide colors.

--- frontend/lib/font_awesome_icons.js
+++ frontend/lib/font_awesome_icons.js
@@ -1,3 +1,4 @@
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
 import { faComments } from '@fortawesome/free-solid-svg-icons/faComments';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
@@ -6,10 +7,12 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyin
 import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons/faSquareFacebook';
 import { faThreads } from '@fortawesome/free-brands-svg-icons/faThreads';
 import { faXTwitter } from '@fortawesome/free-brands-svg-icons/faXTwitter';
+import { faXmark } from '@fortawesome/free-solid-svg-icons/faXmark';
 import { faYoutube } from '@fortawesome/free-brands-svg-icons/faYoutube';
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 
 library.add(
+  faBars,
   faComments,
   faGithub,
   faInstagram,
@@ -18,6 +21,7 @@ library.add(
   faSquareFacebook,
   faThreads,
   faXTwitter,
+  faXmark,
   faYoutube,
 );
 
